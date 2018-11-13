0959454a9201c0f94769bf347e656f2ff5d135e0
jasonhilimire <jasonhilimire@mac.com>
2018-11-13 07:20:16 -0700

    setup cell in cellForRowAt

--- RaceCompare/ViewController.swift
+++ RaceCompare/ViewController.swift
@@ -17,7 +17,10 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.reloadData()
     }
+    
+    //MARK: - Tableview
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -31,13 +34,21 @@ class ViewController: UITableViewController {
         }
         
         let event = events[indexPath.row]
-        //setup labels
+        cell.eventNameLabel.text = "Text \(event.name)"
+        cell.dateLabel.text = "\(String(describing: event.date))"
+//        //cell.percentLabel.text = "\(event.percentcompare)"
+        
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return events.count
+//        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 65
     }
 }
 
